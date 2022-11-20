55113f4a66af73c2e41fdacbf549f23653ab28d9
@li.N <Mr-Developer-again@gmail.com>
2022-11-20 22:58:17 +0330

    added features: 1. creating write_on_the_board() to writing the input string on the board (initial writing) 2. creating function to colorize text on the board

--- src/type_speed.hpp
+++ src/type_speed.hpp
@@ -1,14 +1,40 @@
 #ifndef TYPE_SPEED_H
 #define TYPE_SPEED_H
 
+#include <ncurses.h>
+#include <string>
+#include <vector>
+
+
+typedef unsigned short uint2_t;
+
+
 class type_speed
 {
 public:
-	;
-protected:
-	;
+	virtual ~type_speed() = default;
+	type_speed(WINDOW*, const std::string&);
+
 private:
-	;
+	void window_initializer();
+	
+	// the following function will remove split the given
+	// string based-on space
+	std::vector<std::string> word_spliter(const std::string&) const;
+
+	// the followling function will write the input string on
+	// the taken win
+	void write_on_the_win() const;
+
+	void start_colors() const;
+
+	uint2_t max_y, max_x;
+
+	WINDOW* win;
+
+	// input string after word_spliter(), will
+	// be stored in this vector
+	std::vector<std::string> input_str;
 };
 
 #endif  // TYPE_SPEED_H
